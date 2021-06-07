049160a2d46b6d6b3e53ee864b74e45152737f1d
shantam8 <dscharmen@aol.com>
2021-06-07 16:07:03 +0200

    desktop done

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
-function submitMailAddress() {
-  let address = document.querySelector("#inputMail").value;
+let inputMail = document.querySelector("#inputMail");
+let errorIcon = document.querySelector("#errorIcon");
+let errorMsg = document.querySelector("#errorMsg");
+let validMsg = document.querySelector("#validMsg");
+let submitMsg = document.querySelector("#submitMsg");
+let submitBtn = document.querySelector("#submitBtn");
 
-  console.log(address);
-
+function verifyMailAddress() {
   if (
-    address.match(
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/
+    inputMail.value.match(
+      /^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{1,3}|\d+)$/i
     )
   ) {
-    console.log("passt");
-    document.querySelector("#errorIcon").style.display = "none";
-    document.querySelector("#errorMsg").style.display = "none";
-    document.querySelector("#validMsg").style.display = "block";
+    errorIcon.style.display = "none";
+    errorMsg.style.display = "none";
+    validMsg.style.display = "block";
+    submitMsg.style.display = "none";
+    submitBtn.disabled = false;
   } else {
-    console.log(" bast ed");
-    document.querySelector("#errorIcon").style.display = "inline";
-    document.querySelector("#errorMsg").style.display = "block";
-    document.querySelector("#validMsg").style.display = "none";
+    errorIcon.style.display = "inline";
+    errorMsg.style.display = "block";
+    validMsg.style.display = "none";
+    submitMsg.style.display = "none";
+    submitBtn.disabled = true;
   }
+}
+
+function submitMailAddress() {
+  submitMsg.style.display = "block";
+  validMsg.style.display = "none";
+  submitBtn.disabled = true;
 }
 
 function init() {
   document
     .querySelector("#submitBtn")
     .addEventListener("click", submitMailAddress);
+
+  document
+    .querySelector("#inputMail")
+    .addEventListener("keypress", verifyMailAddress);
+
+  document.querySelector(".inputBox").addEventListener("click", (el) => {
+    inputMail.focus();
+  });
 }
 
 window.addEventListener("load", init);
