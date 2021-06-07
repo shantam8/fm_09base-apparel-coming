b533c336a4fe96e493d71109449d4be70442ac73
shantam8 <dscharmen@aol.com>
2021-06-07 14:50:13 +0200

    mobile done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+function submitMailAddress() {
+  let address = document.querySelector("#inputMail").value;
 
-function init(){
-    console.log("Hi");
+  console.log(address);
+
+  if (
+    address.match(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/
+    )
+  ) {
+    console.log("passt");
+    document.querySelector("#errorIcon").style.display = "none";
+    document.querySelector("#errorMsg").style.display = "none";
+    document.querySelector("#validMsg").style.display = "block";
+  } else {
+    console.log(" bast ed");
+    document.querySelector("#errorIcon").style.display = "inline";
+    document.querySelector("#errorMsg").style.display = "block";
+    document.querySelector("#validMsg").style.display = "none";
+  }
 }
 
+function init() {
+  document
+    .querySelector("#submitBtn")
+    .addEventListener("click", submitMailAddress);
+}
 
 window.addEventListener("load", init);
